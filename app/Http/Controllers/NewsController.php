23e49c7fcef9583e6185a9293e0ecfb85550b5fc
AnggoroSetoo <angg22016ti@student.nurulfikri.ac.id>
2023-11-17 13:19:55 +0700

    UTS Pemrograman Backend 3SE02 - News

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -13,15 +13,19 @@ class NewsController extends Controller
      */
     public function index()
     {
+        //Get All Student
         $news = News::all();
 
+        //Jika Data Ada
         if ($news) {
 			$response = [
 				'message' => 'Get All Resource',
 				'data' => $news,
 			];
 			return response()->json($response, 200);
-		} else {
+		} 
+        //Jika Data kosong
+        else {
 			$response = [
 				'message' => 'Data is Empty'
 			];
@@ -34,17 +38,18 @@ class NewsController extends Controller
      */
     public function store(Request $request)
     {
-         //validasi
+         //validasi data
          $validateData = $request->validate([
-            'title' => 'required',
-            'author' => 'required',
-            'description' => 'required',
-            'content' => 'required',
-            'url' => 'required',
-            'url_image' => 'required',
+            'title' => 'required|min:5|max:255',
+            'author' => 'required|min:3|max:100',
+            'description' => 'required|min:20|max:500',
+            'content' => 'required|min:500|max:2000',
+            'url' => 'required|url|unique:news,url',
+            'url_image' => 'required|url|mimes:jpg,jpeg,png',
             'category' => 'required'
         ]);
 
+        //set tanggal saat ini
         $validateData['published_at'] = Carbon::now();
 
         $news = News::create($validateData);
@@ -64,8 +69,10 @@ class NewsController extends Controller
      */
     public function show($id)
     {
+        //Get ID
         $news = News::find($id);
 
+        //Jika Data Ada
 		if($news){
 			$response = [
 				'message' => 'Get Detail Resource',
@@ -74,6 +81,8 @@ class NewsController extends Controller
 
 			return response()->json($response, 200);
 		}
+
+        //Jika Data Tidak Ada
 		else{
 			$response = [
 				'message' => 'Resource Not Found',
@@ -87,7 +96,10 @@ class NewsController extends Controller
      */
     public function update(Request $request, News $news, $id)
     {
+        //Get ID    
         $news = News::find($id);
+
+        //Jika Data Ada
 		if($news){
 			
 			$news->update($request->all());
@@ -98,6 +110,8 @@ class NewsController extends Controller
 			];
 			return response()->json($response, 200);
 		}
+
+        //Jika Data Tidak Ada
 		else{
 			$response = [
 				'message' => 'Resource Not Found',
@@ -107,11 +121,14 @@ class NewsController extends Controller
     }
 
     /**
-     * Remove the specified resource from storage.
+     * Menghapus Data dari databasi berdasarkan ID
      */
     public function destroy($id)
     {
+        //Get ID 
         $news = News::find($id);
+
+        //Jika Data Ada
 		if($news){
 			$news->delete();
 			$response = [
@@ -119,6 +136,8 @@ class NewsController extends Controller
 			];
 			return response()->json($response, 200);
 		}
+
+        //Jika Data Tidak Ada
 		else{
 			$response = [
 				'message' => 'Resource Not Found',
@@ -129,19 +148,24 @@ class NewsController extends Controller
 
     public function search($title)
     {
+        //Mencari berdasarkan title/author/description/content dengan like 
         $news = News::where('title', 'like', '%' . $title . '%')
         ->orWhere('author', 'like', '%' . $title . '%')
         ->orWhere('description', 'like', '%' . $title . '%')
         ->orWhere('content', 'like', '%' . $title . '%')
         ->get();
 
+        //Jika ada
         if ($news->count()) {
             $resource = [
                 'message' => 'Get Search Resource',
                 'data' => $news,
             ];
             return response()->json($resource, 200);
-        } else {
+        } 
+        
+        //Jika tidak
+        else {
             $data = [
                 'message' => 'Resource Not Found',
             ];
@@ -152,15 +176,20 @@ class NewsController extends Controller
     }
     public function sport()
     {
+        //Mencari data berdasarkan category sport
         $news = News::where('category', 'sport')->get();
 
+        //Jika ada
         if ($news->count()) {
             $resource = [
                 'message' => 'Get Sport Resource',
                 'data' => $news,
             ];
             return response()->json($resource, 200);
-        } else {
+        }
+        
+        //Jika tidak
+        else {
             $data = [
                 'message' => 'No news found for the category "sport"',
             ];
@@ -171,24 +200,32 @@ class NewsController extends Controller
 
     public function finance()
     {
+        //Mencari data berdasarkan category finance
         $news = News::where('category', 'finance')->get();
 
+        //Jika ada
         if ($news->count()) {
             $resource = [
                 'message' => 'Get Finance Resource',
                 'data' => $news,
             ];
             return response()->json($resource, 200);
-        } else {
+        }
+        
+        //Jika tidak
+        else {
             $data = [
                 'message' => 'No news found for the category "finance"',
             ];
+
+            return response()->json($data, 404);
         }
     
     }
 
     public function automotive()
     {
+        //Mencari data berdasarkan category automotive
         $news = News::where('category', 'automotive')->get();
 
         if ($news->count()) {
@@ -197,10 +234,15 @@ class NewsController extends Controller
                 'data' => $news,
             ];
             return response()->json($resource, 200);
-        } else {
+        } 
+        
+        //Jika tidak
+        else {
             $data = [
                 'message' => 'No news found for the category "automotive"',
             ];
+
+            return response()->json($data, 404);
         }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -16,18 +16,38 @@ use App\Http\Controllers\NewsController;
 |
 */
 
-#Login Register
+#Login Register menggunakan library Laravel Sanctum
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
 
+
+#Menggunakan Middleware
 Route::middleware('auth:sanctum')->group(function () {
+
+    //Get All News
     Route::get('/news', [NewsController::class, 'index']);
+
+    //Membuat News
     Route::post('/news', [NewsController::class, 'store']);
+
+    //Melihat Detail sebuah News
     Route::get('/news/{id}', [NewsController::class, 'show']);
+
+    //Update News
     Route::put('/news/{id}', [NewsController::class, 'update']);
+
+    //Menghapus News
     Route::delete('/news/{id}', [NewsController::class, 'destroy']);
+
+    //Mencari News berdasarkan title
     Route::get('/news/search/{title}', [NewsController::class, 'search']);
+
+    //Mencari News berdasarkan category sport
     Route::get('/news/category/sport', [NewsController::class, 'sport']);
+
+    //Mencari News berdasarkan category finance
     Route::get('/news/category/finance', [NewsController::class, 'finance']);
+
+    //Mencari News berdasarkan category automotive
     Route::get('/news/category/automotive', [NewsController::class, 'automotive']);
 });
